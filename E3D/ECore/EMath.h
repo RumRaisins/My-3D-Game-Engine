0ffb596e78dfc2deea73e5f940001d8544b2dd41
ZhuYi <873125305@qq.com>
2017-03-04 23:37:40 +0800

    2017/3/4

--- E3D/ECore/EMath.h
+++ E3D/ECore/EMath.h
@@ -26,6 +26,23 @@ namespace E3D {
 
 	//获取沿X轴旋转变换矩阵
 	//旋转参数为角度，非弧度
+	void GetRotateMatrix44X(EMatrix44 &mat, EFloat degree);
+	//获取沿Y轴旋转变换矩阵
+	//旋转参数为角度，非弧度
+	void GetRotateMatrix44Y(EMatrix44 &mat, EFloat degree);
+	//获取沿Z轴旋转变换矩阵
+	//旋转参数为角度，非弧度
+	void GetRotateMatrix44Z(EMatrix44 &mat, EFloat degree);
+	//沿任意轴旋转
+	//旋转参数为角度，非弧度
+	void GetRotateMatrix44(EMatrix44 &mat, const EVector4D &normal, EFloat degree);
 
-	
+	void GetVertex4DAddVector4D(const EVector4D &v, const EVector4D& p, EVertex4D &result);
+	//3D向量x44矩阵
+	void GetVector3DMulMatrix44(const EVector3D& vec, const EMatrix44&mat, EVector4D &result);
+	//3D向量x44矩阵
+	void GetVector4DMulMatrix44(const EVector4D& vec, const EMatrix44&mat, EVector4D &result);
+
+	void GetVertex3DMulMatrix44(const EVector4D& vec, const EMatrix44&mat, EVertex4D &result);
+
 }
